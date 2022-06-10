550961987e6164d168a7beaf584b8bfea310bbdd
stephen <stephenodumirin@gmail.com>
2022-06-10 14:28:22 +0100

    Initial commit

---
+++ app/src/main/java/com/stdev/takequiz/presentation/SplashFragment.kt
@@ -0,0 +1,21 @@
+package com.stdev.takequiz.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.stdev.takequiz.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
